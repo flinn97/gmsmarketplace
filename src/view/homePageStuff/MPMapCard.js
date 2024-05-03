9c56f17d631da315bfdbca3008ab92dea72a9c8d
IsaacB <iwbcheetah@gmail.com>
2024-05-03 15:31:41 -0600

    publisherLink

--- src/view/homePageStuff/MPMapCard.js
+++ src/view/homePageStuff/MPMapCard.js
@@ -110,8 +110,9 @@ filterFunc={state.pubilsherFilter}
 
             { type: "img", class: "Img-Midsize" },
             { type: "attribute", name: "publisher", class: "DR-Attribute-Item Publisher", },
-            { type: "attribute", name: "title", class: "Bold-Title DR-Attribute-Item" },
-            {type:'custom', custom: DisplayPublisher}, 
+            {type:'custom', custom: DisplayPublisher,}, 
+            { type: "attribute", name: "title", class: "Bold-Title DR-Attribute-Item", style:{marginTop:"-11px",}, },
+            
             { type: "richReader", name:"promotional", class:"DR-Attribute-Item Ellipsis-Text"},
             { name: "See More", class: "DR-Attribute-Item Button-Type1 a", hasLink: true, to: "/purchase/" },
             { type: "prepost", name: "price", class: "DR-Attribute-Item Shimmer", preText:"$", preStyle:{marginRight:"4px", fontSize:"1.2rem", fontFamily:"inria"}, },
--- src/view/homePageStuff/displayPublisher.js
+++ src/view/homePageStuff/displayPublisher.js
@@ -1,20 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default class DisplayPublisher extends Component{
-    constructor(props){
+export default class DisplayPublisher extends Component {
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         debugger
         let obj = this.props.obj;
         let componentList = this.props.app.state.componentList;
         let publisher = componentList.getComponent("publisher", obj.getJson().owner, "_id");
-    return(
-        <div>
-            {publisher.getJson().publisherName}
-            <div>Profile Coming Soon</div>
-        </div>
-    )
+        let app = this.props.app;
+        let dispatch = app.dispatch;
+        let state = app.state;
+        let styles = state.styles;
+
+        console.log(publisher)
+
+        return (
+            <Link 
+            to={"/publisher/"+publisher.getJson().publisherName} 
+            style={{ background:styles.colors.color1+"d5", width:"100%", cursor:"pointer", textUnderlineOffset:"2px",
+            color:styles.colors.color3+"e9", fontFamily:"inria", position:"absolute", top:190,
+            height:"24px", fontSize:"1.2rem", zIndex:"150"}}>
+                <div className='hover-btn-highlight' style={{fontFamily:"inria", paddingLeft:"4px", }}>
+                {publisher.getJson().publisherName}
+                </div>
+                {/* <div>Profile Coming Soon</div> */}
+            </Link>
+        )
     }
 }
