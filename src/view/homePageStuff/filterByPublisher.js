c9c6c18af1a36f5947cf07fab9b29766d39c87b6
taylormdavidson <taylormdavidson@gmail.com>
2024-05-21 18:07:56 -0600

    all

--- src/view/homePageStuff/filterByPublisher.js
+++ src/view/homePageStuff/filterByPublisher.js
@@ -21,30 +21,41 @@ export default class FilterByPublisher extends Component {
     let styles = state.styles;
     let obj = this.props.obj;
     let list = this.props.list;
-  
+
 
     return (
-      <div className='hover-container' style={{ width: "fit-content", fontSize:"1rem", color: styles.colors.colorWhite + "99", 
-      cursor:"pointer", userSelect: "none", textDecoration:"underline", marginBottom:"-13px",
-      alignContent:"center", justifySelf:"center", alignSelf:"center" }}>
-<img src={userIco} alt={"ava"} className="hover-img" style={{width:"34px",}}/>
-    <div className="hover-div" style={{marginTop:"18px"}}>
+      <div className='hover-container' style={{
+        width: "fit-content", fontSize: "1rem", color: styles.colors.colorWhite + "99",
+        cursor: "pointer", userSelect: "none", textDecoration: "underline", marginBottom: "-13px",
+        alignContent: "center", justifySelf: "center", alignSelf: "center"
+      }}>
+        <img src={userIco} alt={"ava"} className="hover-img" style={{ width: "34px", }} />
+        <div className="hover-div" style={{ marginTop: "18px" }}>
 
-    
-    
-    <div className="menu-bubble" style={{ width: "fit-content", flexDirection:"column", display:"flex", marginLeft:"1px"}}>
-      <MapComponent app={app} name="publisher" cells={[{type:"attribute", name: "publisherName", func:(comp)=>{
-          let publisherFunc = (obj)=>{
-            return obj.getJson().owner===comp.getJson()._id
-          }
-          dispatch({pubilsherFilter:publisherFunc});
-      }}]} />
-      {/* List of Partners */}
-      {/* TAYLOR LIST OF PUBLISHER CHECKBOXES, add to a string, you can have multiple checked and see all checked */}
-      <div className="tail" style={{left:11}}></div>
-    </div>
-    </div>
-        
+
+
+          <div className="menu-bubble" style={{ width: "fit-content", flexDirection: "column", display: "flex", marginLeft: "1px" }}>
+            <div onClick={() => {
+              let publisherFunc = (obj) => {
+                return true
+              }
+              dispatch({ pubilsherFilter: publisherFunc });
+
+            }}>Cancel Filter</div>
+            <MapComponent app={app} name="publisher" cells={[{
+              type: "attribute", name: "publisherName", func: (comp) => {
+                let publisherFunc = (obj) => {
+                  return obj.getJson().owner === comp.getJson()._id
+                }
+                dispatch({ pubilsherFilter: publisherFunc });
+              }
+            }]} />
+            {/* List of Partners */}
+            {/* TAYLOR LIST OF PUBLISHER CHECKBOXES, add to a string, you can have multiple checked and see all checked */}
+            <div className="tail" style={{ left: 11 }}></div>
+          </div>
+        </div>
+
       </div>
     );
   }
