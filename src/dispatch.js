6ae2602abb528f007bff19898d6de813e02b4ec2
IsaacB <iwbcheetah@gmail.com>
2024-07-22 13:59:44 -0600

    centered logo

--- src/dispatch.js
+++ src/dispatch.js
@@ -74,30 +74,30 @@ import BuyPopup from './view/buyPageStuff/buyPopup';
  * 
  */
 export default class Dispatch extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  
+
   }
-  componentDidMount(){
-  
+  componentDidMount() {
+
   }
 
 
-  render(){
+  render() {
     let app = this.props.app;
     let state = app.state;
-    let styles =state.styles;
-    let center = window.innerWidth<1200? {
+    let styles = state.styles;
+    let center = window.innerWidth < 1200 ? {
       display: "flex",
-      flexDirection:"column",
-      justifyContent:"center",
-      alignItems:"center"
-    }: undefined
-  return (
-<BrowserRouter>
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center"
+    } : undefined
+    return (
+      <BrowserRouter>
 
-{/* {state.splash&&(<Splash app={app} />)}/ */}
-{/* {(!state.login || state.keepLogin)&&(
+        {/* {state.splash&&(<Splash app={app} />)}/ */}
+        {/* {(!state.login || state.keepLogin)&&(
   <div style={{zIndex: state.keepLogin&& ("-10000"), opacity:state.keepLogin&& ("0"), position: state.keepLogin&&("absolute")}}>
     {window.location.href.includes("signup")?(
 <Register app={app} />
@@ -108,48 +108,51 @@ export default class Dispatch extends Component {
   </div>
 )}
 {state.login&&( */}
-  
-    <div style={{
-      width:"100%", 
-      height:"96vh",
-      }}>
-        {state.user?(
-        <>
-     
-     {/* <Nav  app={app} type="topBarNav"  template="legato"  theme="legatoDark"
+
+        <div style={{
+          width: "100%",
+          height: "96vh",
+        }}>
+          {state.user ? (
+            <>
+
+              {/* <Nav  app={app} type="topBarNav"  template="legato"  theme="legatoDark"
      options={{
       
      }}
   
      
      /> */}
-     {/* //notification: int variable of watching something? Or string pointing to type that gets info from object for notification. Object contains function for notifications, and it goes and interacts with it. Either give it a string or a User Object. */}
-  <div style={{position:"fixed", top:"0px", left:"0px", width:"100vw"}}><img style={{width:"100px", marginLeft:"12px", marginTop:"12px", marginBottom:"12px"}} src={logo}/></div>
-    <div style={{width:"100%", marginTop:"100px"}}>
-    {state.popupSwitch === "buyPopup" &&state.currentComponent!==undefined  &&<BuyPopup type="popup" options={{ cardType: "popupCreate" }} app={app} handleClose={() => { app.dispatch({ popupSwitch: "", currentComponent: undefined, payment:"" }) }}/>}
-     <Routes>
-      {state.switchCase?.map((obj, index)=>
-        <Route path={obj.path} element={<obj.comp app={app}/>} />
-      )}
-      {/* <Route path="/signup" element={<Register app={app} />} /> */}
-      {state.idSwitchCase?.map((obj, index) =>
-                <Route path={obj.path + "/:id"} element={<obj.comp app={app} />} />
+              {/* //notification: int variable of watching something? Or string pointing to type that gets info from object for notification. Object contains function for notifications, and it goes and interacts with it. Either give it a string or a User Object. */}
+              <div style={{ position: "fixed", top: "0px", left: "0px", width: "100vw", justifyContent:"center",padding:"2px", paddingBottom:"11px", textAlign: "center"  }}>
+                <img style={{ width: "100px", marginBottom: "12px" }} src={logo} />
+              </div>
+              <div style={{ width: "100%", marginTop: "100px" }}>
+                {state.popupSwitch === "buyPopup" && state.currentComponent !== undefined && <BuyPopup type="popup" options={{ cardType: "popupCreate" }} app={app} handleClose={() => { app.dispatch({ popupSwitch: "", currentComponent: undefined, payment: "" }) }} />}
+                <Routes>
+                  {state.switchCase?.map((obj, index) =>
+                    <Route path={obj.path} element={<obj.comp app={app} />} />
+                  )}
+                  {/* <Route path="/signup" element={<Register app={app} />} /> */}
+                  {state.idSwitchCase?.map((obj, index) =>
+                    <Route path={obj.path + "/:id"} element={<obj.comp app={app} />} />
 
-              )}
+                  )}
 
-      {state.switchCase?.filter(obj => obj.idLink === true).map((obj, index) =>
-                <Route path={obj.path + "/:id"} element={<obj.idLinkComp app={app} />} />
+                  {state.switchCase?.filter(obj => obj.idLink === true).map((obj, index) =>
+                    <Route path={obj.path + "/:id"} element={<obj.idLinkComp app={app} />} />
 
-              )}
-      
-</Routes>
-</div>
-</>
- ):(<Login app={app}/>)} 
-     </div>
-       {/* )} */}
+                  )}
+
+                </Routes>
+              </div>
+            </>
+          ) : (<Login app={app} />)}
+        </div>
+        {/* )} */}
 
 
-     </BrowserRouter>
-  )}
+      </BrowserRouter>
+    )
+  }
 }
